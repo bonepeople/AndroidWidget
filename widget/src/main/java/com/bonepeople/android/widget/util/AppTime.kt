e7ac6d1807faa2d3f4e28b75a246cfa180f3cc4d
bonepeople <bonepeople@sina.com>
2021-12-31 23:50:41 +0800

    添加AppTime工具类

---
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppTime.kt
@@ -0,0 +1,22 @@
+package com.bonepeople.android.widget.util
+
+import java.text.DateFormat
+
+/**
+ * 时间相关工具
+ */
+object AppTime {
+    /**
+     * 用于格式化时间戳的对象
+     */
+    private val dateFormatter: DateFormat by lazy {
+        DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
+    }
+
+    /**
+     * 将时间戳格式化为"2021/1/1 12:33:33"形式的字符串
+     */
+    fun getDateTimeString(time: Long): String {
+        return dateFormatter.format(time)
+    }
+}
