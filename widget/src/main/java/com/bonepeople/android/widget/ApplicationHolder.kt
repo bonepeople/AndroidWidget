5b23f30e92ab29e319582b9a9f98fef988e0cb61
bonepeople <bonepeople@sina.com>
2022-01-04 16:14:39 +0800

    在ApplicationHolder中添加获取自身APP信息的方法

--- widget/src/main/java/com/bonepeople/android/widget/ApplicationHolder.kt
+++ widget/src/main/java/com/bonepeople/android/widget/ApplicationHolder.kt
@@ -2,10 +2,24 @@ package com.bonepeople.android.widget
 
 import android.app.Application
 import android.content.Context
+import androidx.core.content.pm.PackageInfoCompat
 import androidx.startup.Initializer
 
 object ApplicationHolder {
     lateinit var instance: Application
+    val packageInfo by lazy { instance.packageManager.getPackageInfo(getPackageName(), 0) }
+
+    fun getPackageName(): String {
+        return instance.packageName
+    }
+
+    fun getVersionCode(): Long {
+        return PackageInfoCompat.getLongVersionCode(packageInfo)
+    }
+
+    fun getVersionName(): String {
+        return packageInfo.versionName
+    }
 
     class AppInitializer : Initializer<ApplicationHolder> {
         override fun create(context: Context): ApplicationHolder {
