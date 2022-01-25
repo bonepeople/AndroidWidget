7a9c11791c91358e912a39997774c08e648b5d4f
bonepeople <bonepeople@sina.com>
2022-01-25 22:49:42 +0800

    调整ActivityHolder，添加数据存储逻辑

--- widget/src/main/java/com/bonepeople/android/widget/ActivityHolder.kt
+++ widget/src/main/java/com/bonepeople/android/widget/ActivityHolder.kt
@@ -3,8 +3,10 @@ package com.bonepeople.android.widget
 import android.app.Activity
 import android.os.Bundle
 import java.util.*
+import kotlin.collections.HashMap
 
 object ActivityHolder : DefaultActivityLifecycleCallbacks {
+    private val activityData = HashMap<Activity, HashMap<String, Any>>()
     val activityList = LinkedList<Activity>()
 
     fun getTopActivity(): Activity? {
@@ -17,5 +19,19 @@ object ActivityHolder : DefaultActivityLifecycleCallbacks {
 
     override fun onActivityDestroyed(activity: Activity) {
         activityList.remove(activity)
+        activityData.remove(activity)
+    }
+
+    fun Activity.putExtra(key: String, value: Any) {
+        val map = activityData.getOrPut(this) { HashMap() }
+        map[key] = value
+    }
+
+    fun Activity.getExtra(key: String): Any? {
+        return activityData[this]?.get(key)
+    }
+
+    fun Activity.removeExtra(key: String) {
+        activityData[this]?.remove(key)
     }
 }
