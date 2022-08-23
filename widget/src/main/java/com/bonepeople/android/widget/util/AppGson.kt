2d0ed539706db15e8d7b46dc469770a9d44486bc
bonepeople <bonepeople@sina.com>
2022-08-23 15:30:40 +0800

    优化GsonFactory相关的代码

--- widget/src/main/java/com/bonepeople/android/widget/util/AppGson.kt
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppGson.kt
@@ -6,7 +6,6 @@ import com.google.gson.internal.bind.TreeTypeAdapter
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * Gson数据转换工具类
@@ -37,7 +36,7 @@ object AppGson {
     /**
      * 为gson添加一个可以去除空值的适配器
      * + 该适配器可以在解析的过程中去掉json字符串中的null，防止空指针的发生
-     * @param originGson 需要添加适配器的gson，可以是[AppGson.gson]
+     * @param originGson 需要添加适配器的gson，可以是[AppGson.gson]，去掉null之后将使用此gson进行解析
      * @return 返回一个已经添加了适配器的gson，可以使用返回的gson进行解析
      */
     @CheckResult
@@ -46,29 +45,21 @@ object AppGson {
     }
 
     private class GsonFactory(private val originGson: Gson) : TypeAdapterFactory {
-        private val adapter = GsonAdapter(originGson)
-        override fun <T : Any?> create(gson: Gson?, type: TypeToken<T>?): TypeAdapter<T>? {
+        private val adapter = GsonAdapter()
+        override fun <T : Any?> create(gson: Gson, type: TypeToken<T>): TypeAdapter<T>? {
             return TreeTypeAdapter.newFactoryWithMatchRawType(type, adapter).create(originGson, type)
         }
     }
 
-    private class GsonAdapter(private val gson: Gson) : JsonDeserializer<Any> {
-        override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Any {
-            if (json?.isJsonObject == true) {
-                val obj = json.asJsonObject
-                searchInObject(obj)
-                return gson.fromJson(obj, typeOfT)
-            }
-            if (json?.isJsonArray == true) {
-                val array = json.asJsonArray
-                searchInArray(array)
-                return gson.fromJson(array, typeOfT)
-            }
-            return gson.fromJson(json, typeOfT)
+    private class GsonAdapter : JsonDeserializer<Any> {
+        override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Any {
+            if (json.isJsonObject) searchInObject(json.asJsonObject)
+            if (json.isJsonArray) searchInArray(json.asJsonArray)
+            return context.deserialize(json, typeOfT)
         }
 
         private fun searchInObject(obj: JsonObject) {
-            val nullKey = ArrayList<String>()
+            val nullKey = LinkedList<String>()
             obj.keySet().forEach {
                 val element = obj.get(it)
                 when {
@@ -82,11 +73,11 @@ object AppGson {
 
         private fun searchInArray(arr: JsonArray) {
             val nullIndex = LinkedList<Int>()
-            arr.forEachIndexed { index, jsonElement ->
+            arr.forEachIndexed { index, element ->
                 when {
-                    jsonElement.isJsonNull -> nullIndex.addFirst(index)
-                    jsonElement.isJsonObject -> searchInObject(jsonElement.asJsonObject)
-                    jsonElement.isJsonArray -> searchInArray(jsonElement.asJsonArray)
+                    element.isJsonNull -> nullIndex.addFirst(index)
+                    element.isJsonObject -> searchInObject(element.asJsonObject)
+                    element.isJsonArray -> searchInArray(element.asJsonArray)
                 }
             }
             nullIndex.forEach { arr.remove(it) }
