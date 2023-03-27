e6b4e5f453413752a96c1b249d5564fad83d84d9
bonepeople <bonepeople@sina.com>
2023-03-27 18:59:21 +0800

    更新DateTimePicker的样式及交互效果

--- widget/src/main/java/com/bonepeople/android/widget/view/DateTimePicker.kt
+++ widget/src/main/java/com/bonepeople/android/widget/view/DateTimePicker.kt
@@ -12,6 +12,8 @@ import androidx.fragment.app.FragmentManager
 import com.bonepeople.android.widget.ActivityHolder
 import com.bonepeople.android.widget.databinding.DialogDateTimePickerBinding
 import com.bonepeople.android.widget.util.AppTime
+import com.bonepeople.android.widget.util.AppView.gone
+import com.bonepeople.android.widget.util.AppView.show
 import com.bonepeople.android.widget.util.AppView.singleClick
 import java.util.Calendar
 
@@ -24,6 +26,19 @@ class DateTimePicker(private val manager: FragmentManager) : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View = views.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dialog?.run { setCanceledOnTouchOutside(false) }
+        views.textViewDate.singleClick {
+            views.linearLayoutTime.gone()
+            views.linearLayoutDate.show()
+            views.textViewDate.alpha = 1f
+            views.textViewTime.alpha = 0.5f
+        }
+        views.textViewTime.singleClick {
+            views.linearLayoutDate.gone()
+            views.linearLayoutTime.show()
+            views.textViewDate.alpha = 0.5f
+            views.textViewTime.alpha = 1f
+        }
         views.numberPickerYear.minValue = 1900
         views.numberPickerYear.maxValue = 2099
         views.numberPickerYear.value = calendar[Calendar.YEAR]
@@ -31,33 +46,48 @@ class DateTimePicker(private val manager: FragmentManager) : DialogFragment() {
             if (views.numberPickerMonth.value == 2) {
                 updateMaxDay(newVal, 2)
             }
+            updateTitle()
         }
         views.numberPickerMonth.minValue = 1
         views.numberPickerMonth.maxValue = 12
         views.numberPickerMonth.value = calendar[Calendar.MONTH] + 1
         views.numberPickerMonth.setOnValueChangedListener { _, _, newVal ->
             updateMaxDay(views.numberPickerYear.value, newVal)
+            updateTitle()
         }
         views.numberPickerDay.minValue = 1
         updateMaxDay(views.numberPickerYear.value, views.numberPickerMonth.value)
         views.numberPickerDay.value = calendar[Calendar.DAY_OF_MONTH]
+        views.numberPickerDay.setOnValueChangedListener { _, _, _ ->
+            updateTitle()
+        }
 
         views.numberPickerHour.minValue = 0
         views.numberPickerHour.maxValue = 23
         views.numberPickerHour.setFormatter { AppTime.fillString(it, "00") }
         views.numberPickerHour.value = calendar[Calendar.HOUR_OF_DAY]
+        views.numberPickerHour.setOnValueChangedListener { _, _, _ ->
+            updateTitle()
+        }
 
         views.numberPickerMinute.minValue = 0
         views.numberPickerMinute.maxValue = 59
         views.numberPickerMinute.setFormatter { AppTime.fillString(it, "00") }
         views.numberPickerMinute.value = calendar[Calendar.MINUTE]
+        views.numberPickerMinute.setOnValueChangedListener { _, _, _ ->
+            updateTitle()
+        }
 
         views.numberPickerSecond.minValue = 0
         views.numberPickerSecond.maxValue = 59
         views.numberPickerSecond.setFormatter { AppTime.fillString(it, "00") }
         views.numberPickerSecond.value = calendar[Calendar.SECOND]
+        views.numberPickerSecond.setOnValueChangedListener { _, _, _ ->
+            updateTitle()
+        }
 
-        views.buttonOk.singleClick { onTimeSet() }
+        views.buttonOk.singleClick { submit() }
+        updateTitle()
     }
 
     override fun onResume() {
@@ -72,9 +102,14 @@ class DateTimePicker(private val manager: FragmentManager) : DialogFragment() {
         super.onDestroyView()
     }
 
-    private fun onTimeSet() {
+    private fun updateTitle() {
         calendar.set(views.numberPickerYear.value, views.numberPickerMonth.value - 1, views.numberPickerDay.value, views.numberPickerHour.value, views.numberPickerMinute.value, views.numberPickerSecond.value)
-        calendar[Calendar.MILLISECOND] = 0
+        val title = AppTime.getDateTimeString(calendar.timeInMillis).split(' ')
+        views.textViewDate.text = title[0]
+        views.textViewTime.text = title[1]
+    }
+
+    private fun submit() {
         action.invoke(calendar)
         dismiss()
     }
@@ -91,6 +126,7 @@ class DateTimePicker(private val manager: FragmentManager) : DialogFragment() {
     fun show(time: Long = System.currentTimeMillis(), action: (calendar: Calendar) -> Unit) {
         if (isAdded) return
         if (time > 0) calendar.timeInMillis = time
+        calendar[Calendar.MILLISECOND] = 0
         this.action = action
         closing = false
         show(manager, null)
