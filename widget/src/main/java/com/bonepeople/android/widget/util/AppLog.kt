652a207232f64e3f0f2f5d3b0a3ff551f49fe41f
bonepeople <bonepeople@sina.com>
2023-06-22 14:17:24 +0800

    优化AppLog，允许存在多个实例以支持多个不同tag的输出

--- widget/src/main/java/com/bonepeople/android/widget/util/AppLog.kt
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppLog.kt
@@ -7,44 +7,74 @@ import com.bonepeople.android.widget.ApplicationHolder
  * 日志输出工具类
  */
 @Suppress("UNUSED")
-object AppLog {
-    /**
-     * 默认日志TAG
-     */
-    var tag = "AppLog"
-
+class AppLog(val tag: String) {
     /**
      * 日志输出开关
      */
-    var enable = ApplicationHolder.debug
+    var enable = true
 
     fun verbose(message: Any?, throwable: Throwable? = null) {
-        if (enable) {
+        if (AppLog.enable && enable) {
             Log.v(tag, message.toString(), throwable)
         }
     }
 
     fun debug(message: Any?, throwable: Throwable? = null) {
-        if (enable) {
+        if (AppLog.enable && enable) {
             Log.d(tag, message.toString(), throwable)
         }
     }
 
     fun info(message: Any?, throwable: Throwable? = null) {
-        if (enable) {
+        if (AppLog.enable && enable) {
             Log.i(tag, message.toString(), throwable)
         }
     }
 
     fun warn(message: Any?, throwable: Throwable? = null) {
-        if (enable) {
+        if (AppLog.enable && enable) {
             Log.w(tag, message.toString(), throwable)
         }
     }
 
     fun error(message: Any?, throwable: Throwable? = null) {
-        if (enable) {
+        if (AppLog.enable && enable) {
             Log.e(tag, message.toString(), throwable)
         }
+    }
+
+    companion object {
+        /**
+         * 全局的默认TAG
+         */
+        var defaultTag = "AppLog"
+
+        @Deprecated("use defaultTag instead", ReplaceWith("AppLog.defaultTag"))
+        var tag = defaultTag
+
+        /**
+         * 全局日志输出开关
+         */
+        var enable = ApplicationHolder.debug
+
+        /**
+         * AppLog实例集合
+         */
+        val instances = HashMap<String, AppLog>()
+
+        /**
+         * 获取指定标签的实例
+         * + 未传入参数会返回默认标签的实例
+         * + 创建的实例会保存在[instances]中，供下次使用
+         */
+        fun tag(tag: String = defaultTag): AppLog {
+            return instances.getOrPut(tag) { AppLog(tag) }
+        }
+
+        fun verbose(message: Any?, throwable: Throwable? = null) = tag().verbose(message, throwable)
+        fun debug(message: Any?, throwable: Throwable? = null) = tag().debug(message, throwable)
+        fun info(message: Any?, throwable: Throwable? = null) = tag().info(message, throwable)
+        fun warn(message: Any?, throwable: Throwable? = null) = tag().warn(message, throwable)
+        fun error(message: Any?, throwable: Throwable? = null) = tag().error(message, throwable)
     }
 }
