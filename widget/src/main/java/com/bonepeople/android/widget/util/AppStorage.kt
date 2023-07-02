deddb24606219bb8a05c2365f17341bcb31c8d3c
bonepeople <bonepeople@sina.com>
2023-07-02 15:29:38 +0800

    升级AppStorage的存储逻辑
    将数据实例的初始化方式由defaultMMKV改为mmkvWithID，防止与其他依赖库冲突

--- widget/src/main/java/com/bonepeople/android/widget/util/AppStorage.kt
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppStorage.kt
@@ -14,29 +14,58 @@ import java.io.OutputStream
  */
 @Suppress("UNUSED")
 object AppStorage {
-    private val storage: MMKV by lazy { init() }
+    private const val TYPE_STRING = 1
+    private const val TYPE_BOOLEAN = 2
+    private const val TYPE_INT = 3
+    private const val TYPE_LONG = 4
+    private const val TYPE_FLOAT = 5
+
+    /**
+     * 原始存储实例
+     * + 该实例用于将旧数据迁移至新的存储实例中，从1.5.5版本，开始过渡阶段
+     * + 预计于1.6.0版本，移除
+     * + 过渡阶段会从该实例中读取数据，但不会写入
+     */
+    private val originStorage: MMKV by lazy { init() }
+    private val dataStorage: MMKV by lazy {
+        MMKV.initialize(ApplicationHolder.app, MMKVLogLevel.LevelNone)
+        val secret: String = AppMessageDigest.md5(ApplicationHolder.getPackageName())
+        MMKV.mmkvWithID("${ApplicationHolder.getPackageName()}.data", MMKV.MULTI_PROCESS_MODE, secret).also {
+            it.trim()
+        }
+    }
+    private val keyStorage: MMKV by lazy {
+        MMKV.initialize(ApplicationHolder.app, MMKVLogLevel.LevelNone)
+        val secret: String = AppMessageDigest.md5(ApplicationHolder.getPackageName())
+        MMKV.mmkvWithID("${ApplicationHolder.getPackageName()}.key", MMKV.MULTI_PROCESS_MODE, secret).also {
+            it.trim()
+        }
+    }
 
     /**
      * 多进程支持
      * + 在使用[AppStorage]前通过修改此项来调整多进程支持，默认为启用
+     * + 在某些特殊情况下可能会无效，已于1.5.5版本标记为废弃
+     * + 预计于1.6.0版本，移除
      */
+    @Deprecated("multiProcess在某些特殊情况下可能会无效，现在全部的数据实例均采用多进程方式初始化")
     var multiProcess = true
 
     private fun init(): MMKV {
         MMKV.initialize(ApplicationHolder.app, MMKVLogLevel.LevelNone)
-        val mode = if (multiProcess) MMKV.MULTI_PROCESS_MODE else MMKV.SINGLE_PROCESS_MODE
         val secret: String = AppMessageDigest.md5(ApplicationHolder.getPackageName())
         var mmkv: MMKV? = null
         var version = 0
-        val master = MMKV.mmkvWithID("AppStorage", mode, secret)
+        val master = MMKV.mmkvWithID("AppStorage", MMKV.MULTI_PROCESS_MODE, secret)
         version = master.getInt("AppStorage.version", version)
         if (version == 0) {
-            mmkv = MMKV.defaultMMKV(mode, null)
-            mmkv?.reKey(secret)
-            version = 1
+            mmkv = MMKV.defaultMMKV(MMKV.MULTI_PROCESS_MODE, null)
+        }
+        if (version == 1) {
+            mmkv = MMKV.defaultMMKV(MMKV.MULTI_PROCESS_MODE, secret)
         }
         if (mmkv == null) {
-            mmkv = MMKV.defaultMMKV(mode, secret)
+            mmkv = MMKV.defaultMMKV(MMKV.MULTI_PROCESS_MODE, secret)
         }
         master.encode("AppStorage.version", version)
         mmkv!!.trim()
@@ -47,7 +76,9 @@ object AppStorage {
      * 移除指定字段
      */
     fun remove(key: String) {
-        storage.removeValueForKey(key)
+        originStorage.removeValueForKey(key)
+        dataStorage.removeValueForKey(key)
+        keyStorage.removeValueForKey(key)
     }
 
     /**
@@ -56,7 +87,6 @@ object AppStorage {
     fun backup(target: OutputStream) {
         val file = File(ApplicationHolder.app.cacheDir, "AppStorage.backup")
         file.mkdirs()
-        storage.sync()
         MMKV.backupAllToDirectory(file.absolutePath)
         AppZip.zipFiles(target, file)
         file.listFiles()?.forEach {
@@ -71,7 +101,6 @@ object AppStorage {
     fun restore(source: InputStream) {
         val file = File(ApplicationHolder.app.cacheDir, "AppStorage.backup")
         AppZip.unzipFile(source, file.parentFile!!)
-        storage.sync()
         MMKV.restoreAllFromDirectory(file.absolutePath)
         file.listFiles()?.forEach {
             it.delete()
@@ -83,69 +112,171 @@ object AppStorage {
      * 存放字符串
      */
     fun putString(key: String, value: String?) {
-        storage.putString(key, value)
+        putDataInternal(key, value.toString())
     }
 
     /**
      * 取出字符串，默认为空字符串
      */
     fun getString(key: String, default: String = ""): String {
-        return storage.getString(key, default) ?: default
+        return getDataInternal(key, default)
     }
 
     /**
      * 存放布尔数据
      */
     fun putBoolean(key: String, value: Boolean) {
-        storage.putBoolean(key, value)
+        putDataInternal(key, value)
     }
 
     /**
      * 取出布尔数据，默认为false
      */
     fun getBoolean(key: String, default: Boolean = false): Boolean {
-        return storage.getBoolean(key, default)
+        return getDataInternal(key, default)
     }
 
     /**
      * 存放整型数字
      */
     fun putInt(key: String, value: Int) {
-        storage.putInt(key, value)
+        putDataInternal(key, value)
     }
 
     /**
      * 取出整型数字，默认为0
      */
     fun getInt(key: String, default: Int = 0): Int {
-        return storage.getInt(key, default)
+        return getDataInternal(key, default)
     }
 
     /**
      * 存放长整型数字
      */
     fun putLong(key: String, value: Long) {
-        storage.putLong(key, value)
+        putDataInternal(key, value)
     }
 
     /**
      * 取出长整型数字，默认为0
      */
     fun getLong(key: String, default: Long = 0): Long {
-        return storage.getLong(key, default)
+        return getDataInternal(key, default)
     }
 
     /**
      * 存放浮点型数字
      */
     fun putFloat(key: String, value: Float) {
-        storage.putFloat(key, value)
+        putDataInternal(key, value)
     }
 
     /**
      * 取出浮点型数字，默认为0
      */
     fun getFloat(key: String, default: Float = 0f): Float {
-        return storage.getFloat(key, default)
+        return getDataInternal(key, default)
+    }
+
+    private fun <T> putDataInternal(key: String, value: T) {
+        originStorage.removeValueForKey(key)
+        when (value) {
+            is String -> {
+                dataStorage.putString(key, value)
+                keyStorage.putInt(key, TYPE_STRING)
+            }
+
+            is Boolean -> {
+                dataStorage.putBoolean(key, value)
+                keyStorage.putInt(key, TYPE_BOOLEAN)
+            }
+
+            is Int -> {
+                dataStorage.putInt(key, value)
+                keyStorage.putInt(key, TYPE_INT)
+            }
+
+            is Long -> {
+                dataStorage.putLong(key, value)
+                keyStorage.putInt(key, TYPE_LONG)
+            }
+
+            is Float -> {
+                dataStorage.putFloat(key, value)
+                keyStorage.putInt(key, TYPE_FLOAT)
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> getDataInternal(key: String, default: T): T {
+        var data: T = default
+        when (default) {
+            is String -> {
+                if (originStorage.containsKey(key)) {
+                    data = (originStorage.decodeString(key, default) ?: default) as T
+                    putDataInternal(key, data)
+                } else {
+                    if (keyStorage.decodeInt(key) == TYPE_STRING) {
+                        data = (dataStorage.decodeString(key, default) ?: default) as T
+                    } else {
+                        throw IllegalArgumentException("数据类型错误")
+                    }
+                }
+            }
+
+            is Boolean -> {
+                if (originStorage.containsKey(key)) {
+                    data = originStorage.decodeBool(key, default) as T
+                    putDataInternal(key, data)
+                } else {
+                    if (keyStorage.decodeInt(key) == TYPE_BOOLEAN) {
+                        data = dataStorage.decodeBool(key, default) as T
+                    } else {
+                        throw IllegalArgumentException("数据类型错误")
+                    }
+                }
+            }
+
+            is Int -> {
+                if (originStorage.containsKey(key)) {
+                    data = originStorage.decodeInt(key, default) as T
+                    putDataInternal(key, data)
+                } else {
+                    if (keyStorage.decodeInt(key) == TYPE_INT) {
+                        data = dataStorage.decodeInt(key, default) as T
+                    } else {
+                        throw IllegalArgumentException("数据类型错误")
+                    }
+                }
+            }
+
+            is Long -> {
+                if (originStorage.containsKey(key)) {
+                    data = originStorage.decodeLong(key, default) as T
+                    putDataInternal(key, data)
+                } else {
+                    if (keyStorage.decodeInt(key) == TYPE_LONG) {
+                        data = dataStorage.decodeLong(key, default) as T
+                    } else {
+                        throw IllegalArgumentException("数据类型错误")
+                    }
+                }
+            }
+
+            is Float -> {
+                if (originStorage.containsKey(key)) {
+                    data = originStorage.decodeFloat(key, default) as T
+                    putDataInternal(key, data)
+                } else {
+                    if (keyStorage.decodeInt(key) == TYPE_FLOAT) {
+                        data = dataStorage.decodeFloat(key, default) as T
+                    } else {
+                        throw IllegalArgumentException("数据类型错误")
+                    }
+                }
+            }
+        }
+        return data
     }
 }
