a5407303bea00398f6ff1f81845280c613ea77d4
bonepeople <bonepeople@sina.com>
2023-11-28 23:29:11 +0800

    精简AppKeyboard中显示隐藏键盘所需的参数

--- widget/src/main/java/com/bonepeople/android/widget/util/AppKeyboard.kt
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppKeyboard.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.view.postDelayed
+import com.bonepeople.android.widget.ActivityHolder
 
 /**
  * 软键盘工具类
@@ -34,10 +35,17 @@ object AppKeyboard {
      * 为一个输入框显示软键盘
      * @param editText 需要输入内容的输入框
      */
-    fun showKeyboard(context: Context, editText: EditText) {
+    @Deprecated("Use showKeyboard(editText: EditText) instead", ReplaceWith("AppKeyboard.showKeyboard(editText)"))
+    fun showKeyboard(context: Context, editText: EditText) = showKeyboard(editText)
+
+    /**
+     * 为一个输入框显示软键盘
+     * @param editText 需要输入内容的输入框
+     */
+    fun showKeyboard(editText: EditText) {
         editText.postDelayed(200) {
             editText.requestFocus()
-            val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val inputMethodManager = editText.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
         }
     }
@@ -46,7 +54,14 @@ object AppKeyboard {
      * 隐藏软键盘
      * @param focusedView 当前获取焦点的控件
      */
-    fun hideKeyboard(context: Context, focusedView: View?) {
+    @Deprecated("Use hideKeyboard() instead", ReplaceWith("AppKeyboard.hideKeyboard()"))
+    fun hideKeyboard(context: Context, focusedView: View?) = hideKeyboard()
+
+    /**
+     * 隐藏软键盘
+     */
+    fun hideKeyboard() {
+        val focusedView = ActivityHolder.getTopActivity()?.window?.currentFocus
         focusedView?.run {
             clearFocus()
             val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
