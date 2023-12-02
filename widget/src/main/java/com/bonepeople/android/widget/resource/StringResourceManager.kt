a2f3f23a6451f53ab6aebf38c6fa79c654fa803f
bonepeople <bonepeople@sina.com>
2023-12-02 12:48:44 +0800

    更新StringResourceManager的注释，对使用方法进行详细说明

--- widget/src/main/java/com/bonepeople/android/widget/resource/StringResourceManager.kt
+++ widget/src/main/java/com/bonepeople/android/widget/resource/StringResourceManager.kt
@@ -4,8 +4,13 @@ import java.util.Locale
 
 /**
  * 字符串资源管理器
+ * + 使用方法：
+ * 1. 定义字符串模版，继承自[StringTemplate]，并实现[StringTemplate.templateClass]属性，此模版可定义为抽象类或接口，可以为不同模块或页面定义不同的字符串模版，在模版中定义需要被子类实现的变量
+ * 2. 定义字符串实例，继承自字符串模版，实现具体的字符串内容，同一个模版可以定义多个不同语言版本的字符串实例
+ * 3. 在使用字符串之前，调用[register]方法注册字符串实例
+ * 4. 在需要使用字符串的地方，调用[get]方法获取字符串实例
  */
-@Suppress("unused")
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 object StringResourceManager {
     private val localStrings = HashMap<String, LinkedHashMap<String, LinkedHashMap<String, StringTemplate>>>()
 
@@ -13,7 +18,7 @@ object StringResourceManager {
      * 注册字符串实例
      * + 以先后顺序关系确定默认展示的字符串，在未找到匹配的字符串时优先展示先注册的字符串
      * @param strings 字符串实例
-     * @param locales 语言环境，可传入多个语言环境，将同一字符串实例注册到多个语言环境中
+     * @param locales 语言环境，可传入多个语言环境，将同一字符串实例注册到多个语言环境中，`Locale.US`
      */
     fun register(strings: StringTemplate, vararg locales: Locale) {
         locales.forEach { locale: Locale ->
@@ -31,8 +36,8 @@ object StringResourceManager {
     /**
      * 获取指定语言环境对应的字符串实例
      * + 未找到指定语言环境的字符串实例时，将返回注册的第一个语言环境的第一个字符串实例
-     * @param templateClass 字符串模版类型
-     * @param locale 语言环境，默认为系统语言环境
+     * @param templateClass 字符串模版类型，`StringTemplate::class.java`
+     * @param locale 语言环境，默认为系统语言环境，`Locale.US`
      */
     @Suppress("unchecked_cast")
     fun <T : StringTemplate> get(templateClass: Class<T>, locale: Locale = Locale.getDefault()): T {
--- widget/src/main/java/com/bonepeople/android/widget/resource/StringTemplate.kt
+++ widget/src/main/java/com/bonepeople/android/widget/resource/StringTemplate.kt
@@ -6,15 +6,16 @@ package com.bonepeople.android.widget.resource
 interface StringTemplate {
     /**
      *  字符串模板类型，用于区分不同的字符串模板，实现类需要复写此方法
+     *  + 使用类型而不用字符串，可以避免类型重复导致字符串实例被覆盖，也可以避免字符串的拼写错误
      */
     val templateClass: Class<out StringTemplate>
 
+    /**
+     * 字符串模版的伴生对象
+     * + 此为示例代码，开发者定义字符串模版时可参考此写法，方便在获取字符串对象时传递类型
+     */
     @Suppress("unused")
     companion object {
-        /**
-         * 字符串模板类型
-         * + 在伴生对象中定义，方便用户直接获取类型
-         */
         val templateClass: Class<StringTemplate> = StringTemplate::class.java
     }
 }
