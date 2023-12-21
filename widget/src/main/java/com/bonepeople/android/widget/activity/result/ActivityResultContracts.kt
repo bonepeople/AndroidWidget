dff9d6ae21503ad5f5faf25b43ae6d88b2c6b859
bonepeople <bonepeople@sina.com>
2023-12-21 22:32:11 +0800

    记录IntentLauncher的使用记录并在崩溃时输出

--- widget/src/main/java/com/bonepeople/android/widget/activity/result/ActivityResultContracts.kt
+++ widget/src/main/java/com/bonepeople/android/widget/activity/result/ActivityResultContracts.kt
@@ -1,11 +1,13 @@
 package com.bonepeople.android.widget.activity.result
 
+import android.app.Activity
 import android.content.Intent
 import com.bonepeople.android.widget.ActivityHolder
 import com.bonepeople.android.widget.ActivityHolder.getExtra
 
 /**
  * 启动[Intent]并返回[IntentResult]，用于获取Activity的返回结果
+ * + 内部使用了[Activity.startActivityForResult]方法，会导致标记为singleTop、singleTask、singleInstance的Activity创建新的实例，调用时请手动为intent添加flag
  */
 fun Intent.launch(): IntentResult {
     val data = ActivityHolder.getTopActivity()?.getExtra("com.bonepeople.android.widget.activity.result.IntentLauncher")
@@ -15,7 +17,8 @@ fun Intent.launch(): IntentResult {
         }?.let {
             return (it as IntentLauncher).launch(this)
         }
-        throw IllegalStateException("同时使用的IntentLauncher数量超出上限，可以通过IntentLauncher.initialCapacity调整初始化的上限")
+        val recordLog: String = IntentLauncher.stackTraceToString()
+        throw IllegalStateException("同时使用的IntentLauncher数量超出上限，可以通过IntentLauncher.initialCapacity调整初始化的上限\n$recordLog")
     } else {
         throw IllegalStateException("${ActivityHolder.getTopActivity()?.javaClass}需要继承自ComponentActivity才能使用此方法")
     }
--- widget/src/main/java/com/bonepeople/android/widget/activity/result/IntentLauncher.kt
+++ widget/src/main/java/com/bonepeople/android/widget/activity/result/IntentLauncher.kt
@@ -16,12 +16,14 @@ import com.bonepeople.android.widget.DefaultActivityLifecycleCallbacks
  * + 在每个[ComponentActivity]创建的时候都会配套创建[IntentLauncher]，用于启动用户指定的[Intent]
  */
 class IntentLauncher private constructor() : ActivityResultCallback<ActivityResult> {
+    private var hostName: String = ""
     private var launcher: ActivityResultLauncher<Intent>? = null
     private var intentResult: IntentResult? = null
 
     internal fun launch(intent: Intent): IntentResult {
         val tempResult = IntentResult()
         intentResult = tempResult
+        launchRecord[this] = "$hostName:IntentLauncher@${System.identityHashCode(this).toString(36)}:$intent"
         launcher?.launch(intent)
         return tempResult
     }
@@ -29,18 +31,28 @@ class IntentLauncher private constructor() : ActivityResultCallback<ActivityResu
     internal fun ready() = intentResult == null
 
     override fun onActivityResult(result: ActivityResult) {
+        launchRecord.remove(this)
         val tempResult = intentResult
         intentResult = null
         tempResult?.saveResult(result)
     }
 
     companion object {
+        private val launchRecord = LinkedHashMap<IntentLauncher, String>()
+
         /**
          * IntentLauncher的初始化数量
          * + 该变量用于控制在每个Activity中初始化IntentLauncher的数量
          * + 过多的初始化IntentLauncher会造成资源的浪费，请根据需求进行调整
          */
         var initialCapacity = 1
+
+        /**
+         * 获取IntentLauncher调用栈
+         */
+        fun stackTraceToString(): String {
+            return launchRecord.values.reversed().joinToString(separator = "\n")
+        }
     }
 
     internal object Registry : DefaultActivityLifecycleCallbacks {
@@ -49,6 +61,7 @@ class IntentLauncher private constructor() : ActivityResultCallback<ActivityResu
                 val launcherList = ArrayList<IntentLauncher>()
                 for (i in 1..initialCapacity) {
                     val intentLauncher = IntentLauncher()
+                    intentLauncher.hostName = activity.toString()
                     intentLauncher.launcher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult(), intentLauncher)
                     launcherList.add(intentLauncher)
                 }
