27beda1eb70239001a58a1ead687ad41bfff2f27
bonepeople <bonepeople@sina.com>
2024-01-11 21:49:53 +0800

    在AppLog输出的日志中添加调用栈的信息，将静态方法标记为弃用

--- widget/src/main/java/com/bonepeople/android/widget/util/AppLog.kt
+++ widget/src/main/java/com/bonepeople/android/widget/util/AppLog.kt
@@ -13,6 +13,16 @@ class AppLog(val tag: String) {
      */
     var enable = true
 
+    /**
+     * 是否在日志信息中添加当前调用方法位置的信息
+     */
+    var showStackInfo = false
+
+    /**
+     * 在显示调用栈信息时获取的调用栈位置，需要根据不同情况手动调整
+     */
+    var stackOffset = 5
+
     fun verbose(message: Any?, throwable: Throwable? = null) {
         if (AppLog.enable && enable) {
             Log.v(tag, message.toString(), throwable)
@@ -21,25 +31,37 @@ class AppLog(val tag: String) {
 
     fun debug(message: Any?, throwable: Throwable? = null) {
         if (AppLog.enable && enable) {
-            Log.d(tag, message.toString(), throwable)
+            Log.d(tag, addStackInfoIfNeed(message), throwable)
         }
     }
 
     fun info(message: Any?, throwable: Throwable? = null) {
         if (AppLog.enable && enable) {
-            Log.i(tag, message.toString(), throwable)
+            Log.i(tag, addStackInfoIfNeed(message), throwable)
         }
     }
 
     fun warn(message: Any?, throwable: Throwable? = null) {
         if (AppLog.enable && enable) {
-            Log.w(tag, message.toString(), throwable)
+            Log.w(tag, addStackInfoIfNeed(message), throwable)
         }
     }
 
     fun error(message: Any?, throwable: Throwable? = null) {
         if (AppLog.enable && enable) {
-            Log.e(tag, message.toString(), throwable)
+            Log.e(tag, addStackInfoIfNeed(message), throwable)
+        }
+    }
+
+    private fun addStackInfoIfNeed(message: Any?): String {
+        return if (showStackInfo) {
+            val stack = Thread.currentThread().stackTrace.getOrNull(stackOffset)
+            val className = stack?.className?.split(".")?.last() ?: "UnknownClass"
+            val methodName = stack?.methodName ?: "UnknownMethod"
+            val lineNumber = stack?.lineNumber ?: 0
+            "$message @ $className.$methodName:$lineNumber"
+        } else {
+            message.toString()
         }
     }
 
@@ -48,6 +70,11 @@ class AppLog(val tag: String) {
          * 全局的默认TAG
          */
         var defaultTag = "AppLog"
+
+        /**
+         * 全局默认日志输出对象
+         */
+        val defaultLog: AppLog get() = tag(defaultTag)
 
         /**
          * 全局日志输出开关
@@ -61,17 +88,25 @@ class AppLog(val tag: String) {
 
         /**
          * 获取指定标签的实例
-         * + 未传入参数会返回默认标签的实例
          * + 创建的实例会保存在[instances]中，供下次使用
          */
-        fun tag(tag: String = defaultTag): AppLog {
+        fun tag(tag: String): AppLog {
             return instances.getOrPut(tag) { AppLog(tag) }
         }
 
-        fun verbose(message: Any?, throwable: Throwable? = null) = tag().verbose(message, throwable)
-        fun debug(message: Any?, throwable: Throwable? = null) = tag().debug(message, throwable)
-        fun info(message: Any?, throwable: Throwable? = null) = tag().info(message, throwable)
-        fun warn(message: Any?, throwable: Throwable? = null) = tag().warn(message, throwable)
-        fun error(message: Any?, throwable: Throwable? = null) = tag().error(message, throwable)
+        @Deprecated("推荐使用对象的方法输出日志", ReplaceWith("AppLog.defaultLog.verbose(message, throwable)", "com.bonepeople.android.widget.util.AppLog"))
+        fun verbose(message: Any?, throwable: Throwable? = null) = defaultLog.verbose(message, throwable)
+
+        @Deprecated("推荐使用对象的方法输出日志", ReplaceWith("AppLog.defaultLog.debug(message, throwable)", "com.bonepeople.android.widget.util.AppLog"))
+        fun debug(message: Any?, throwable: Throwable? = null) = defaultLog.debug(message, throwable)
+
+        @Deprecated("推荐使用对象的方法输出日志", ReplaceWith("AppLog.defaultLog.info(message, throwable)", "com.bonepeople.android.widget.util.AppLog"))
+        fun info(message: Any?, throwable: Throwable? = null) = defaultLog.info(message, throwable)
+
+        @Deprecated("推荐使用对象的方法输出日志", ReplaceWith("AppLog.defaultLog.warn(message, throwable)", "com.bonepeople.android.widget.util.AppLog"))
+        fun warn(message: Any?, throwable: Throwable? = null) = defaultLog.warn(message, throwable)
+
+        @Deprecated("推荐使用对象的方法输出日志", ReplaceWith("AppLog.defaultLog.error(message, throwable)", "com.bonepeople.android.widget.util.AppLog"))
+        fun error(message: Any?, throwable: Throwable? = null) = defaultLog.error(message, throwable)
     }
 }
